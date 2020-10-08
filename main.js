693bb1e38e43c9c6a62518a001a732c214f866c0
Mateus Anjos <mateus.a.anjos@gmail.com>
2020-10-07 23:08:55 -0300

    Add bot reply to hello

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-
+const prefix = '-';
 
 
 function on_ready() {
     console.log('DicordBot is ONLINE');
 }
 
-client.once('ready', on_ready)
+function on_message_received(message) {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'hello') {
+        message.channel.send('Hello friend!');
+    }
+}
+
+client.on('message', message => on_message_received(message));
+
+client.once('ready', on_ready);
 
 
 
