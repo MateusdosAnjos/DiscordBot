4521631eeb5b0907ac8a40d46a9b29cd7f587e3b
Mateus Anjos <mateus.a.anjos@gmail.com>
2020-10-07 22:32:09 -0300

    Add file to make the bot go ONLINE

---
+++ main.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+
+
+client.once('ready', ()=> {
+    console.log('DicordBot is ONLINE');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('INSERT YOUR BOT TOKEN HERE!');
